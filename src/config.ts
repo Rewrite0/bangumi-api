c13301d3414bba4bfb945ba5468b3c6d32785384
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2023-04-09 14:12:04 +0800

    3.0.0

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const URL = 'https://api.bgm.tv';
-export const VERSION = '2.0.0';
+export const VERSION = '3.0.0';
 export const UserAgent = `Rewrite0/BangumiApi/${VERSION} (https://github.com/Rewrite0/BangumiApi)`;
